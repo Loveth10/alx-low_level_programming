31e66f0e0db0d503b61b1dc12aa494cb00b59159
loveth10 <lovethokebe@gmail.com>
2022-06-03 14:11:26 -0700

    long-task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+alx-low_level_programming/0x00-hello_world/4-puts.c
+@Ifeoluwa19
+Ifeoluwa19 4-6task
+Latest commit db121de yesterday
+ History
+ 1 contributor
+Executable File  11 lines (11 sloc)  181 Bytes
+   
+#include<stdio.h>
+/**
+* main - program prints with function puts
+*
+*Return: Always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
